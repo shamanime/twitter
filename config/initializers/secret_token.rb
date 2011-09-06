ad8de0b6c7888d4e769b069c192f074df0b45359
Jefferson Queiroz Venerando <jefferson@shamanime.com>
2011-09-06 12:01:20 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '396ebeb7ea694363def0d385053b374629b9cfedebac2b91d8dc7c095b2b17ce8fbb11e0d5d5ebfca7c8f98f8e8826ccddd6dadf2374727bab5543a89b66e8e8'
