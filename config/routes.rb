75a3195d7719b9d6c0f4539fec963e19e5c86f42
Jefferson Queiroz Venerando <jefferson@shamanime.com>
2011-09-06 14:26:39 -0300

    Refactoring routing, adding navigations and more pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Twitter::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
   resources :microposts
   resources :users
@@ -55,7 +55,7 @@ Twitter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
