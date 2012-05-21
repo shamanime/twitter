2500e4c50c16dc4910eecd3d191891d6ba8fb539
Jefferson Queiroz Venerando <jefferson@shamanime.com>
2012-05-21 11:34:09 -0300

    Atualizando

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate(params[:session][:email],
-                             params[:session][:password])
+                  params[:session][:password])
     if user.nil?
       flash.now[:error] = "Invalid email/password combination."
       @title = "Sign in"
